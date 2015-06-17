5c1a0e7e82651018988a65979e86be1e0fd7b5b2
Kevin Martensson <kevinmartensson@gmail.com>
2015-06-17 20:19:36 +0200

    Add partial Windows support
    Fixes #4.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 'use strict';
+var execFile = require('child_process').execFile;
+var toPercent = require('to-percent');
 
 if (process.platform === 'darwin') {
 	module.exports = require('osx-brightness');
 } else if (process.platform === 'linux') {
 	module.exports = require('xdg-brightness');
 } else {
-	module.exports = function () {
-		throw new Error('Only OS X and Linux systems are supported');
+	module.exports.set = function (val, cb) {
+		if (typeof val !== 'number' || val < 0 || val > 1) {
+			throw new Error('Expected a number between 0 and 1');
+		}
+
+		execFile('./nircmd.exe', ['setbrightness', toPercent(val)], function (err) {
+			if (err) {
+				cb(err);
+				return;
+			}
+
+			cb();
+		});
+	};
+
+	module.exports.get = function () {
+		throw new Error('Getting brightness is only supported on OS X and Linux systems');
 	};
 }
